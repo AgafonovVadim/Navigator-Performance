78e7fc8d552c6e240e22303050c3b125c22e0c41
Vadim Agafonov <113839834+AgafonovVadim@users.noreply.github.com>
2025-02-24 17:02:10 +0300

    Removed: unsupported tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -13,21 +13,4 @@ describe('AppController', () => {
 
     appController = app.get<AppController>(AppController);
   });
-
-  describe('getIndexPage', () => {
-    it('should return unauthorized session when auth is not "true"', () => {
-      const result = appController.getIndexPage('false');
-      expect(result).toEqual({ session: { loggedIn: false } });
-    });
-    it('should return authorized session when auth is "true"', () => {
-      const result = appController.getIndexPage('true');
-      expect(result).toEqual({
-        session: { username: 'Vadim', loggedIn: true },
-      });
-    });
-    it('should return unauthorized session when auth is undefined', () => {
-      const result = appController.getIndexPage('');
-      expect(result).toEqual({ session: { loggedIn: false } });
-    });
-  });
 });
